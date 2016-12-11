5c2ad6507195c31ed2785cfd4e7f1421205f8d9a
Aventurer <molivertauler@gmail.com>
2016-12-11 11:48:26 +0100

    in process

--- src/Game/Renderer.hpp
+++ src/Game/Renderer.hpp
@@ -1,17 +1,25 @@
+#pragma once
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include "Window.hpp"
 
 #define R Renderer::Instance()
 
 class Renderer
 {
+	SDL_Renderer *myRenderer{ nullptr };
 public:
 	static Renderer& Instance()
 	{
 		static Renderer inst;
 		return inst;
 	}
+	SDL_Renderer* operator() (void) { return myRenderer; }
 
 private:
-	Renderer() {}
+	Renderer() {
+		myRenderer = SDL_CreateRenderer(W(), -1, 0);
+	}
 	Renderer(const Renderer&);
 	Renderer& operator=(const Renderer&);
 };
--- src/Game/Window.hpp
+++ src/Game/Window.hpp
@@ -6,16 +6,19 @@
 
 class Window {
 	Window(const std::string &&name, int &&screenWidth, int &&screenHeight) : w_name(name), w_width(screenWidth), w_height(screenHeight) {
+		myWindow = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, 0);
 	}
 public:
-	inline static Window &Instance(std::string &&name = "", int &&screenWidth = 0, int &&screenHeight = 0) {
-		static Window window(std::move(name), std::move(screenWidth), std::move(screenHeight));
+	inline static Window &Instance() {
+		static Window window("", 0, 0);
 		return window;
 	}
 	~Window() {
 		SDL_DestroyWindow(myWindow), myWindow = nullptr; //Destroy window
 		SDL_Quit();
 	}
+	inline SDL_Window* operator() (void) { return myWindow; }
+
 private:
 	SDL_Window* myWindow{ nullptr };
 	const int w_width, w_height;
