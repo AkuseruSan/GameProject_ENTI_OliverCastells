6d9b7fe7b31c80375a39777edd9b0af265d2e3f7
Aventurer <molivertauler@gmail.com>
2017-01-10 22:47:08 +0100

    IM

--- src/Game/InputManager.cpp
+++ src/Game/InputManager.cpp
@@ -3,7 +3,7 @@
 
 InputManager::InputManager()
 {
-
+	click = false;
 }
 
 InputManager::~InputManager()
@@ -12,13 +12,13 @@ InputManager::~InputManager()
 }
 
 void InputManager::Update(bool& isRun) {
-	click = false;
 	SDL_Event evnt;
 	while (SDL_PollEvent(&evnt)) {
 		switch (evnt.type) {
 		case SDL_QUIT:		isRun = false;	break;
 		case SDL_MOUSEMOTION:	mousePos = { evnt.motion.x, evnt.motion.y };		break;
-		case SDL_MOUSEBUTTONUP: click = true; break;
+		case SDL_MOUSEBUTTONDOWN: click = true; break;
+		case SDL_MOUSEBUTTONUP: click = false; break;
 		case SDL_KEYDOWN:
 			switch (evnt.key.keysym.sym) {
 			case SDLK_UP:			if (direction != DIR_DOWN)	direction = DIR_UP;		break;
