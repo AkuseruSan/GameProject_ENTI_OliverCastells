3499b0a4dc1da2d9a2477b7dcadb44f17de3a451
AkuseruSan <axelcastells.gamedev@gmail.com>
2017-01-10 22:22:26 +0100

    FIX

--- src/Game/InputManager.cpp
+++ src/Game/InputManager.cpp
@@ -17,14 +17,14 @@ void InputManager::Update(bool& isRun) {
 	while (SDL_PollEvent(&evnt)) {
 		switch (evnt.type) {
 		case SDL_QUIT:		isRun = false;	break;
+		case SDL_MOUSEMOTION:	mousePos = { evnt.motion.x, evnt.motion.y };		break;
+		case SDL_MOUSEBUTTONUP: click = true; break;
 		case SDL_KEYDOWN:
 			switch (evnt.key.keysym.sym) {
 			case SDLK_UP:			if (direction != DIR_DOWN)	direction = DIR_UP;		break;
 			case SDLK_DOWN:			if (direction != DIR_UP)	direction = DIR_DOWN;	break;
 			case SDLK_LEFT:			if (direction != DIR_RIGHT)	direction = DIR_LEFT;	break;
 			case SDLK_RIGHT:		if (direction != DIR_LEFT)	direction = DIR_RIGHT;	break;
-			case SDL_MOUSEMOTION:	mousePos = { evnt.motion.x, evnt.motion.y };		break;
-			case SDL_MOUSEBUTTONUP: click = true; break;
 			}
 		}
 	}
